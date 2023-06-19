ddd14ad5ec484921cdca002d4c3e3d13e7d2bb31
Mani <43148406+AdonissMani@users.noreply.github.com>
2023-06-19 18:04:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,43 @@
 
-  document.addEventListener("DOMContentLoaded", function() {
-    const colorButton = document.getElementById("new-quote");
-    
+document.addEventListener("DOMContentLoaded", function() {
+  const colorButton = document.getElementById("new-quote");
+  getRandomQuotes();
+  colorButton.addEventListener("click", function() {
+    changeColor();
     getRandomQuotes();
-    
-    colorButton.addEventListener("click", function() {
-      changeColor();
-      getRandomQuotes();
-    });
-
-    function changeColor() {
-      const randomColor = getRandomColor();
-      quote.style.color = randomColor;
-      body.style.backgroundColor = randomColor;
-      colorButton.style.color = randomColor;
-      author.style.color = randomColor;
-      coma.style.color= randomColor;
-    }
-  
-    function getRandomColor() {
-      const letters = "0123456789ABCDEF";
-      let color = "#";
-      for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-      }
-      return color;
-    }
-    function getRandomQuotes(){
-      // const randomNumber = Math.floor(Math.random() * 1001);
-      fetch('https://api.quotable.io/random')
-      .then(response => response.json())
-      .then(data => {
-        // Use the quote in your website
-            const quoteElement = document.getElementById('quote');
-            const authorElement = document.getElementById('author');
-    
-            // Display the quote and author
-            quoteElement.textContent = data.content;
-            authorElement.textContent = "- " + data.author;
-          })
-          .catch(error => console.error(error));
-    }
-    
-    
-    
   });
+
+  function changeColor() {
+    const randomColor = getRandomColor();
+    quote.style.color = randomColor;
+    body.style.backgroundColor = randomColor;
+    colorButton.style.color = randomColor;
+    author.style.color = randomColor;
+    coma.style.color= randomColor;
+  }
+
+  function getRandomColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+  function getRandomQuotes(){
+    // const randomNumber = Math.floor(Math.random() * 1001);
+    fetch('https://api.quotable.io/random')
+    .then(response => response.json())
+    .then(data => {
+      // Use the quote in your website
+          const quoteElement = document.getElementById('quote');
+          const authorElement = document.getElementById('author');
   
+          // Display the quote and author
+          quoteElement.textContent = data.content;
+          authorElement.textContent = "- " + data.author;
+        })
+        .catch(error => console.error(error));
+  }
+  
+});
